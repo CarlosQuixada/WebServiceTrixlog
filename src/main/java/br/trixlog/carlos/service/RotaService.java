96f38a56db1b230634a3745994f31958ebec94d8
CarlosQuixada <carlosquixada95@gmail.com>
2017-06-28 22:42:46 -0300

    Corrigindo Poblema DataBase None Mongo

--- src/main/java/br/trixlog/carlos/service/RotaService.java
+++ src/main/java/br/trixlog/carlos/service/RotaService.java
@@ -2,24 +2,29 @@ package br.trixlog.carlos.service;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.trixlog.carlos.model.Parada;
 import br.trixlog.carlos.model.Rota;
+import br.trixlog.carlos.repository.RotaRepository;
 
 @Service
 public class RotaService {
-
+	@Autowired
+	private RotaRepository rotaRepository;
+	
 	public void gerarRota(List<Parada> stops) {
 
 	}
 
-	public void cadastrarRota(Rota rota) {
-
+	public Rota cadastrarRota(Rota rota) {
+		return rotaRepository.save(rota);
 	}
 
-	public Rota getRota(Long rotaId) {
-		return null;
+	public Rota getRota(String rotaId) {
+		return rotaRepository.findOne(rotaId);
 	}
 
 	public List<Rota> getAllRotas() {
